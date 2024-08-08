386cbf648db278507dcd792d08af1ba9910d6416
EZN429 <00001120212509sp@al.educacao.sp.gov.br>
2024-08-08 15:42:23 -0300

    Add files via upload

---
+++ atividade de pagamento.py
@@ -0,0 +1,11 @@
+opcaopagamento =str(input("qual é a opção de pagamento"))
+if opcaopagamento=="credito":
+  print ("por favor insira ou aproxime o cartão de  credito") 
+elif opcaopagamento=="debito":
+  print ("por favor insira ou aproxim o cartão de debito")
+elif opcaopagamento=="pix":
+  print ("por favor escaneie o codigo QRcode")
+elif opcaopagamento=="dinhero":
+   print("pague no caixa e aguarde o troco")
+else:
+   print ("pagamento invalido")
